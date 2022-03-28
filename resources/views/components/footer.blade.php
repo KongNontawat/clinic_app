baf8fd2525ac6d02a0a8e7b97e07a010c0b9dd33
Itsnielz <96enjoy.v2@gmail.com>
2022-03-28 15:03:37 +0700

    home

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,75 +1,63 @@
 <!-- Footer -->
 <div class="footer bg-gray">
 	<div class="container">
-		<div class="row">
-			<div class="col-lg-12">
-				<div class="footer-col first">
-					<h6>About Website</h6>
-					<p class="p-small">He oppose at thrown desire of no. Announcing impression unaffected day his are unreserved indulgence. Him hard find read are you</p>
-				</div> <!-- end of footer-col -->
-				<div class="footer-col second">
-					<h6>Links</h6>
-					<ul class="list-unstyled li-space-lg p-small">
-						<li>Important: <a href="terms.html">Terms & Conditions</a>, <a href="privacy.html">Privacy Policy</a></li>
-						<li>Useful: <a href="#">Colorpicker</a>, <a href="#">Icon Library</a>, <a href="#">Illustrations</a></li>
-						<li>Menu: <a href="#header">Home</a>, <a href="#details">Details</a>, <a href="#services">Services</a>, <a href="#contact">Contact</a></li>
-					</ul>
-				</div> <!-- end of footer-col -->
-				<div class="footer-col third">
-					<span class="fa-stack">
-						<a href="#your-link">
-							<i class="fas fa-circle fa-stack-2x"></i>
-							<i class="fab fa-facebook-f fa-stack-1x"></i>
-						</a>
-					</span>
-					<span class="fa-stack">
-						<a href="#your-link">
-							<i class="fas fa-circle fa-stack-2x"></i>
-							<i class="fab fa-twitter fa-stack-1x"></i>
-						</a>
-					</span>
-					<span class="fa-stack">
-						<a href="#your-link">
-							<i class="fas fa-circle fa-stack-2x"></i>
-							<i class="fab fa-pinterest-p fa-stack-1x"></i>
-						</a>
-					</span>
-					<span class="fa-stack">
-						<a href="#your-link">
-							<i class="fas fa-circle fa-stack-2x"></i>
-							<i class="fab fa-instagram fa-stack-1x"></i>
-						</a>
-					</span>
-					<p class="p-small">We would love to hear from you <a href="mailto:contact@site.com"><strong>contact@site.com</strong></a></p>
-				</div> <!-- end of footer-col -->
-			</div> <!-- end of col -->
-		</div> <!-- end of row -->
-	</div> <!-- end of container -->
-</div> <!-- end of footer -->
-<!-- end of footer -->
+		<div class="d-flex justify-content-center">
+			<div class="row">
+				<div class="col-lg-4 col-12 mb-5">
+					<h3>เมนู</h3>
+					<a href="">Home</a><br>
+					<a href="">Promotion</a><br>
+					<a href="">Blog</a><br>
+					<a href="">Review</a><br>
+					<a href="">Services</a><br>
+					<a href="">Contact</a>
+				</div>
+
+				<div class="col-lg-4 col-12 mb-5">
+					<h3>ติดต่อเรา</h3>
+					<p>Tel : 0999999999 <br>
+						E-mail : ookko@email.com
+					</p>
+					<h3 class="mt-3">Social Media</h3>
+					<div class="d-flex">
+						<a href="#!"><i class="bi bi-facebook me-2" style="font-size: 20px;"></i></a>
+						<a href="#!"><i class="bi bi-line me-2" style="font-size: 20px;"></i></a>
+						<a href="#!"><i class="bi bi-instagram me-2" style="font-size: 20px;"></i></a>
+					</div>
+				</div>
+
+				<div class="col-lg-4 col-12 mb-5">
+					<h3>เกี่ยวกับเรา</h3>
+					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque ab error quasi ea, nemo, nihil mollitia fugiat voluptates, totam sapiente repudiandae assumenda tempore. Esse, quos? Quisquam quae ut voluptates cum!</p>
+				</div>
+			</div>
+		</div>
+	</div>
 
 
-<!-- Copyright -->
-<div class="copyright bg-gray">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-12">
-				<p class="p-small">Copyright © <a href="#your-link">Your name</a></p>
-			</div> <!-- end of col -->
-		</div> <!-- enf of row -->
 
-		<div class="row">
-			<div class="col-lg-12">
-				<p class="p-small">Distributed By <a href="https://themewagon.com/">Themewagon</a></p>
-			</div> <!-- end of col -->
-		</div> <!-- enf of row -->
-	</div> <!-- end of container -->
-</div> <!-- end of copyright -->
-<!-- end of copyright -->
+	<!-- Copyright -->
+	<div class="copyright bg-gray">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12">
+					<p class="p-small">Copyright © <a href="#your-link">Your name</a></p>
+				</div> 
+			</div> 
+
+			<div class="row">
+				<div class="col-lg-12">
+					<p class="p-small">Distributed By <a href="https://themewagon.com/">Themewagon</a></p>
+				</div> 
+			</div> 
+		</div> 
+	</div> 
 
 
-<!-- Back To Top Button -->
-<button onclick="topFunction()" id="myBtn">
-	<i class="fa-solid fa-angle-up text-white"></i>
-</button>
-<!-- end of back to top button -->
+
+	<!-- Back To Top Button -->
+	<button onclick="topFunction()" id="myBtn">
+		<i class="fa-solid fa-angle-up text-white"></i>
+	</button>
+	<!-- end of back to top button -->
+</div>
